9567324359be67f1d526c25b1d08e965397f8081
joel1844 <friascastillo0403@gmail.com>
2021-12-16 13:54:50 -0400

    Fixing Conexion

--- Defensordelpueblo_bot.py
+++ Defensordelpueblo_bot.py
@@ -7,6 +7,7 @@ from os import path
 import datetime
 import pandas as pd
 import nest_asyncio
+import time
 nest_asyncio.apply()
 
 # Enable logging
@@ -26,7 +27,6 @@ def start(update: Update, context: CallbackContext) -> None:
         fr'Hi {user.mention_markdown_v2()}\!',
         reply_markup=ForceReply(selective=True),
     )
-
 
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
@@ -76,30 +76,35 @@ def tweet(update:Update, context:CallbackContext):
 
 
 def main() -> None:
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    updater = Updater("2009767025:AAHeClxGi3tpiX55ki6PIQVTquLBUq1Vu1s")
+    try:
+        """Start the bot."""
+        # Create the Updater and pass it your bot's token.
+        updater = Updater("2009767025:AAHeClxGi3tpiX55ki6PIQVTquLBUq1Vu1s")
 
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+        # Get the dispatcher to register handlers
+        dispatcher = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("tweet", tweet))
-    dispatcher.add_handler(CommandHandler("echo", echo))
+        # on different commands - answer in Telegram
+        dispatcher.add_handler(CommandHandler("start", start))
+        dispatcher.add_handler(CommandHandler("help", help_command))
+        dispatcher.add_handler(CommandHandler("tweet", tweet))
+        dispatcher.add_handler(CommandHandler("echo", echo))
 
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, tweet))
+        # on non command i.e message - echo the message on Telegram
+        dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, tweet))
 
-    # Start the Bot
-    updater.start_polling()
+        # Start the Bot
+        updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
+        # Run the bot until you press Ctrl-C or the process receives SIGINT,
+        # SIGTERM or SIGABRT. This should be used most of the time, since
+        # start_polling() is non-blocking and will stop the bot gracefully.
+        updater.idle()
+    except Exception as e:
+        time.sleep(15)
+        print(e)
+        print("Error de internet, esperando 120 segundos")
+        main()
 
 if __name__ == '__main__':
     main()
